fd6d4f674a10ed91ea44a272085d09b87bdf8f39
Bao Nguyen <ba339794@ucf.edu>
2026-02-15 23:12:27 -0500

    Implement API endpoints: 1. Login.php: handles GET, POST requests for user logins and signups 2. AddColor.php: handles POST requests for adding colors to the user's account 3. SearchColors.php: handles GET requests for searching colors AVAILABLE ONLY in the user's account

---
+++ api/AddColor.php
@@ -0,0 +1,46 @@
+<?php
+
+	header('Access-Control-Allow-Origin: *');
+	header('Access-Control-Allow-Method: GET, POST');
+	header('Access-Control-Allow-Header: Content-Type');
+	header('Content-Type: application/json');
+
+	$inData = getRequestInfo();
+	
+	$color = $inData["color"];
+	$userId = $inData["userId"];
+
+	$localhost = process.en
+	$conn = new mysqli("localhost", "username", "password", "db_name");		// Configure each argument to your droplet's credentials and database
+	if ($conn->connect_error) 
+	{
+		returnWithError( $conn->connect_error );
+	} 
+	else
+	{
+		$stmt = $conn->prepare("INSERT into Colors (UserId,Name) VALUES(?,?)");
+		$stmt->bind_param("ss", $userId, $color);
+		$stmt->execute();
+		$stmt->close();
+		$conn->close();
+		returnWithError("");
+	}
+
+	function getRequestInfo()
+	{
+		return json_decode(file_get_contents('php://input'), true);
+	}
+
+	function sendResultInfoAsJson( $obj )
+	{
+		header('Content-type: application/json');
+		echo $obj;
+	}
+	
+	function returnWithError( $err )
+	{
+		$retValue = '{"error":"' . $err . '"}';
+		sendResultInfoAsJson( $retValue );
+	}
+	
+?>
